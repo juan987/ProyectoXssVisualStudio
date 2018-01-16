cff86f453c5a661c68140f12bef41ad470355347
Alumno <Alumno@FALCON>
2018-01-16 20:07:31 +0100

    Add project files.

---
+++ ProyectoXssVisualStudio/Controllers/ValidacionesController.cs
@@ -0,0 +1,34 @@
+﻿using ProyectoXssVisualStudio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoXssVisualStudio.Controllers
+{
+    public class ValidacionesController : Controller
+    {
+        // GET: Validaciones
+        public ActionResult Index()
+        {
+            Empleado emp = new Empleado();
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Empleado emp)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Correcto");
+            }
+            return View(emp);
+        }
+
+        public ActionResult Correcto()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoXssVisualStudio/Models/Empleado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoXssVisualStudio.Models
+{
+    public class Empleado
+    {
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public string Nombre { set; get; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public string Apellidos { set; get; }
+        [EmailAddress(ErrorMessage = "El correo electrónico no es válido")]
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public string Email { set; get; }
+        [DataType(DataType.Currency, ErrorMessage = "Introduzca un número")]
+        public long Salario { set; get; }
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public string Pass { set; get; }
+        [Compare("Pass", ErrorMessage = "El dato no coincide con el password")]
+        public string Pass2 { set; get; }
+
+    }
+}
